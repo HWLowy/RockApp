66722173b87018cfaa86825a2222cadef1fe622f
Bright Levi Bennett <bright.bennett@icloud.com>
2023-05-01 16:00:33 -1000

    Initial Commit

---
+++ Rock App/Rock_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_AppApp.swift
+//  Rock App
+//
+//  Created by Bert Bennett on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct Rock_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
